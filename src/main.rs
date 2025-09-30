68350bca2d740c29ec6784bb09f4e84b19913093
Brian-Conley <brianjconleyjr@gmail.com>
2025-09-30 19:33:04 -0400

    Add request to get a list of all Steam apps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use serde::{Deserialize};
+use reqwest;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let appsurl: String = String::from(
+        "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+    );
+
+    let steamapps: SteamApps = reqwest::get(appsurl)
+        .await?
+        .json()
+        .await?;
+
+    println!("{:?}", steamapps);
+
+    Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+struct AppInfo {
+    appid: u32,
+    name: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Applist {
+    apps: Vec<AppInfo>,
+}
+
+#[derive(Debug, Deserialize)]
+struct SteamApps {
+    applist: Applist,
 }
